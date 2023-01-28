82e8958453cd783a0f8b439762ed841087be8bc8
clifford-devops6 <devops.6@clifford.co.ke>
2023-01-28 09:25:34 +0300

    personalize

--- resources/views/pages/account/personalize/index.vue
+++ resources/views/pages/account/personalize/index.vue
@@ -2,44 +2,46 @@
     <Head><title>Personalize Feeds</title></Head>
 <reader>
     <template #sidebar>
-        <personalize-menu></personalize-menu>
+        <personalize-menu @categoriesFilter="switchCategory" @StreamsFilter="switchStream"></personalize-menu>
     </template>
     <div class="my-10">
-        <div class="flex justify-between px-3">
-            <div>
-                <h1 class="font-bold text-xl">Streams</h1>
+        <div v-if="showStreams">
+            <div class="flex justify-between px-3">
+                <div>
+                    <h1 class="font-bold text-xl">Streams</h1>
+                </div>
+                <div class="flex gap-4">
+                    <button @click="enterpriseModal=true" v-if="auth.role==='Enterprise'"  class="font-bold text-primary-100"><span class="mr-2"><i class="fal fa-pen"></i></span>Customize enterprise</button>
+                    <button @click="personalSource=true" class="font-bold text-primary-100"><span class="mr-2"><i class="fal fa-pen"></i></span>Customize Personal</button>
+                </div>
             </div>
-            <div class="flex gap-4">
-                <button @click="enterpriseModal=true" v-if="auth.role==='Enterprise'"  class="font-bold text-primary-100"><span class="mr-2"><i class="fal fa-pen"></i></span>Customize enterprise</button>
-                <button @click="personalSource=true" class="font-bold text-primary-100"><span class="mr-2"><i class="fal fa-pen"></i></span>Customize Personal</button>
-            </div>
-        </div>
-        <div class="my-8">
-            <div class="block">
-                <div v-for="source in account.data.sources"
-                     class="p-2 m-1 inline-block border rounded-lg text-center bg-white">
-                    <div class="grid justify-center">
-                        <img :src="source.logo" :alt="source.name" class="h-8 place-self-center">
-                        <h6 class="">{{ source.name }}</h6>
-                        <div>
-                            <span class="text-[9px] bg-primary-300 text-white p-1 rounded">Enterprise</span>
+            <div class="my-8">
+                <div class="block">
+                    <div v-for="source in account.data.sources"
+                         class="p-2 m-1 inline-block border rounded-lg text-center bg-white">
+                        <div class="grid justify-center">
+                            <img :src="source.logo" :alt="source.name" class="h-8 place-self-center">
+                            <h6 class="">{{ source.name }}</h6>
+                            <div>
+                                <span class="text-[9px] bg-primary-300 text-white p-1 rounded">Enterprise</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="block">
+
+                    <div v-for="source in user.data.sources"
+                         class="p-2 m-1 inline-block border rounded-lg text-center bg-white">
+                        <div class="grid justify-center">
+                            <img :src="source.logo" :alt="source.name" class="h-8 place-self-center">
+                            <h6 >{{ source.name }}</h6>
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="block">
-
-                <div v-for="source in user.data.sources"
-                     class="p-2 m-1 inline-block border rounded-lg text-center bg-white">
-                    <div class="grid justify-center">
-                        <img :src="source.logo" :alt="source.name" class="h-8 place-self-center">
-                        <h6 >{{ source.name }}</h6>
-                    </div>
-                </div>
-            </div>
         </div>
 
-        <div class="mt-16">
+        <div class="mt-16" v-if="showCategories">
             <div class="flex justify-between px-3">
                 <div>
                     <h1 class="font-bold text-xl">Categories</h1>
@@ -104,6 +106,21 @@ const enterpriseModal=ref(false)
 const personalSource=ref(false)
 const personalCategory=ref(false)
 const enterpriseCategory=ref(false)
+
+//streams and categories filter
+
+const showStreams=ref(true)
+const showCategories=ref(true)
+
+const switchCategory=()=>{
+    showCategories.value=true;
+    showStreams.value=false
+}
+const switchStream=()=>{
+    showStreams.value=true
+    showCategories.value=false;
+
+}
 </script>
 
 <style scoped>
